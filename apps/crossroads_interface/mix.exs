1d9c91a4cc66cac9ee548ae5c78880f87de59685
Matt Silbernagel <silbermm@gmail.com>
2017-02-15 15:23:42 -0500

    add new version variable

--- apps/crossroads_interface/mix.exs
+++ apps/crossroads_interface/mix.exs
@@ -3,7 +3,7 @@ defmodule CrossroadsInterface.Mixfile do
 
   def project do
     [app: :crossroads_interface,
-     version: append_revision("0.0.3"),
+     version: System.get_env("BUILD_NUMBER") || "0.0.1",
      elixir: "~> 1.0",
      elixirc_paths: elixirc_paths(Mix.env),
      compilers: [:phoenix, :gettext] ++ Mix.compilers,
