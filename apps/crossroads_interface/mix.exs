ef9c794dc4a5f3b5de293190b6c0956e9cc6bf20
Matt Silbernagel <silbermm@gmail.com>
2017-02-15 15:56:31 -0500

    Use timestamp as release number

--- apps/crossroads_interface/mix.exs
+++ apps/crossroads_interface/mix.exs
@@ -3,7 +3,7 @@ defmodule CrossroadsInterface.Mixfile do
 
   def project do
     [app: :crossroads_interface,
-     version: System.get_env("BUILD_NUMBER") || "0.0.1",
+     version: System.get_env("CURRENT_TIMESTAMP") || "0.0.1",
      elixir: "~> 1.0",
      elixirc_paths: elixirc_paths(Mix.env),
      compilers: [:phoenix, :gettext] ++ Mix.compilers,
