3c3d6b943a47967f73c2e8f4aa2df706d14f7faf
Richie Brees <Brees@MacBook-Pro-4.local>
2017-03-03 15:59:31 -0500

    Fixed routing to /connect

--- apps/crossroads_interface/web/controllers/crds_connect_controller.ex
+++ apps/crossroads_interface/web/controllers/crds_connect_controller.ex
@@ -7,14 +7,17 @@ defmodule CrossroadsInterface.CrdsConnectController do
   This controller handles "/connect" (Finder) requests
   """
 
+  plug CrossroadsInterface.Plug.BaseHref, "/connect"
   plug :put_layout, "crds_connect.html"
 
+  
+
   def index(conn, _params) do
-    render conn, "index.html", %{"base_href": "connect", "js_files": [
+    render conn, "index.html", %{ "js_files": [
         "/js/crds_connect/polyfills.js",
         "/js/crds_connect/vendor.js",
         "/js/crds_connect/app.js"
-      ]}
+      ], "base_href": "/connect"}
   end
 
 end
