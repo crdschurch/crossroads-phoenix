01313a630230372ca34c962e0808224a8f05b753
Michael Vezzani <mvezzani@callibrity.com>
2017-03-08 11:07:09 -0500

    US6959 Javascript for Header in Phoenix and CMS
    Javascript that manipulates header now lives in phoenix app.js file. As
    part of this work, the javascript was also added inline in the CMS to
    prove that javascript in the CMS can be run after a page loads.

--- apps/crossroads_interface/web/static/js/app.js
+++ apps/crossroads_interface/web/static/js/app.js
@@ -1,7 +1,14 @@
-import "phoenix_html"
+//import "phoenix_html"
 
 // include all svg files in icons directory in the body of the document
 //var files = require.context('../icons', false, /.svg$/);
 //files.keys().forEach(files);
 
 // import socket from "./socket"
+
+export var App = {
+  run: function(){
+    var pastoralCare = document.getElementById("pastoralCare");
+    pastoralCare.addEventListener("click", function() { alert("Hello from Phoenix JavaScript!"); });
+  }
+}
