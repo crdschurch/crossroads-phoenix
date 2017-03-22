98c8c99e1aa4bfe15429396d15a60397e1b021ae
Richie Brees <Brees@MacBook-Pro-4.local>
2017-03-22 14:46:58 -0400

    Graceful handling of "onload" JS

--- apps/crossroads_interface/web/static/js/app.js
+++ apps/crossroads_interface/web/static/js/app.js
@@ -6,24 +6,38 @@ import Handlebars from "handlebars"
 
 // import socket from "./socket"
 function run() {
+    translateBaseHref();
     test1();
-    alertCMS();
+    invokeAlertCms();
+}
+
+function invokeAlertCms() {
+    if(typeof alertCms !== 'undefined'){
+        alertCms();
+    }
 }
 
 function test1() {
     var test1 = document.getElementById("test1");
-    test1.addEventListener("click", function (event) {
-        event.preventDefault();
-        alert("Hello from Phoenix JavaScript!");
-    });
+    if (test1 !== undefined && test1 !== null) {
+        test1.addEventListener("click", function (event) {
+            event.preventDefault();
+            alert("Hello from Phoenix JavaScript!");
+        });
+    }
+}
+
+function translateBaseHref() {
+    // This could be used to replace {{ }} variables in the nav menu
+    // FWIW I don't like it!
+    var source = document.getElementById("nav-menu");
+    if (source) {
+        var sourceHtml = source.innerHTML;
+        var template = Handlebars.compile(sourceHtml);
+        var html = template({ "base_href": "http://localhost:4000" }) // replace localhost with real base url
+        document.getElementById("nav-menu").innerHTML = html
+    }
+
 }
 
 window.onload = run;
-
-
-// This could be used to replace {{ }} variables in the nav menu
-// FWIW I don't like it!
-var source = document.getElementById("nav-menu").innerHTML;
-var template = Handlebars.compile(source);
-var html = template({ "base_href": "http://localhost:4000" }) // replace localhost with real base url
-document.getElementById("nav-menu").innerHTML = html
