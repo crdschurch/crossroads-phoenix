2d84a9663d59d4f5a64aebc7ea007aa2958735d2
Richie Brees <Brees@MacBook-Pro-4.local>
2017-03-23 09:59:22 -0400

    Remove reference to "handlebars"

--- apps/crossroads_interface/web/static/js/app.js
+++ apps/crossroads_interface/web/static/js/app.js
@@ -1,12 +1,4 @@
-import Handlebars from "handlebars"
-//import "phoenix_html"
-// include all svg files in icons directory in the body of the document
-//var files = require.context('../icons', false, /.svg$/);
-//files.keys().forEach(files);
-
-// import socket from "./socket"
 function run() {
-    translateBaseHref();
     test1();
     invokeAlertCms();
 }
@@ -25,19 +17,6 @@ function test1() {
             alert("Hello from Phoenix JavaScript!");
         });
     }
-}
-
-function translateBaseHref() {
-    // This could be used to replace {{ }} variables in the nav menu
-    // FWIW I don't like it!
-    var source = document.getElementById("nav-menu");
-    if (source) {
-        var sourceHtml = source.innerHTML;
-        var template = Handlebars.compile(sourceHtml);
-        var html = template({ "base_href": "http://localhost:4000" }) // replace localhost with real base url
-        document.getElementById("nav-menu").innerHTML = html
-    }
-
 }
 
 window.onload = run;
